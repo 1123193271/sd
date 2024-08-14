4074dd697dff3a7d68adbf80a3b22da1e5894a07
xyli15 <xyli15@amarsoft.com>
2024-08-14 14:31:08 +0800

    feat:测试提交

---
+++ src/main/java/com/study/xyl/_25_Copy/BeanCopyKit.java
@@ -0,0 +1,43 @@
+package com.study.xyl._25_Copy;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.FatalBeanException;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeanCopyKit {
+
+    public static void copyProperties(Object source, Object target) {
+        Class<?> actualEditable = target.getClass();
+        PropertyDescriptor[] propertyDescriptors = BeanUtils.getPropertyDescriptors(actualEditable);
+        if (source instanceof Map<?,?>) {
+            if (target instanceof Map<?,?>) {
+                ((Map<?, ?>) target).putAll((Map) source);
+            } else {
+                Map<String, Object> map = (Map<String, Object>) source;
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    PropertyDescriptor targetPd = org.springframework.beans.BeanUtils.getPropertyDescriptor(target.getClass(), key);
+                    Method writeMethod = targetPd.getWriteMethod();
+                }
+            }
+        }
+        System.out.println(Arrays.toString(propertyDescriptors));
+
+    }
+
+    public static void main(String[] args) {
+        ExamplePO examplePO = new ExamplePO();
+        Map<String, String> example = new HashMap<>();
+        example.put("id", "001");
+//        example.put("code", "001");
+        example.put("name", "小红");
+        example.put("grade", "100分");
+        example.put("age", "22");
+        copyProperties(example, examplePO);
+    }
+}
